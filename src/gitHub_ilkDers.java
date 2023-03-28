42a62dba55bca3d136a6611d4f5c1d74fad1d83c
mehmetsaidpelit <you@example.com>
2023-03-28 16:23:07 -0300

    230327

--- src/gitHub_ilkDers.java
+++ src/gitHub_ilkDers.java
@@ -1,5 +1,7 @@
 public class gitHub_ilkDers {
     public static void main(String[] args) {
-        System.out.println("Hello Github");
+        System.out.println("Hello Github :)");
+        System.out.println("==============");
+        System.out.println("");
     }
 }
