d0853f3f8e70a853d4f8ab0c2b09c3f32b4882f0
mehmetsaidpelit <you@example.com>
2023-03-28 16:40:06 -0300

    ikinci version

--- src/gitHub_ilkDers.java
+++ src/gitHub_ilkDers.java
@@ -2,6 +2,6 @@ public class gitHub_ilkDers {
     public static void main(String[] args) {
         System.out.println("Hello Github :)");
         System.out.println("==============");
-        System.out.println("");
+        System.out.println("merhaba");
     }
 }
